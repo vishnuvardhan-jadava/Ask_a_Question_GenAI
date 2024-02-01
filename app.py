285fb2b09371809624af8a4f258e328a29c27ffb
Vishnuvardhan Jadava <83878754+vishnuvardhan-jadava@users.noreply.github.com>
2024-02-01 00:36:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import google.generativeai as genai
 
-API_KEY = 'AIzaSyBsTUgZ2q2WPJyxtDF_9lDmkzhwPXlwPvE'
+API_KEY = '' # API key goes here
 
 genai.configure(api_key=API_KEY)
 
 st.set_page_config(page_title='Q and A with Google Gemini-Pro')
 st.header('Enter your question')
-user_input = st.text_input(label='Ask gemini pro any question(no history chat history is maintained)', label_visibility='hidden',placeholder='enter text')
+user_input = st.text_input(label='Ask Gemini pro any question(no history chat history is maintained)', label_visibility='hidden',placeholder='enter text')
 submit = st.button('Ask')
 model = genai.GenerativeModel('gemini-pro')
 if user_input and submit:
